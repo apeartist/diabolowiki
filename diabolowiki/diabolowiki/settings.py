93cce69991f7868b80ae2fdfda3ceb9b5480bebd
appletun091305 <apeartistemail@gmail.com>
2025-04-09 10:43:30 -0400

    Revert "auditlog added"
    This reverts commit 8488fcfb965f363f22c8a74ce05c178df09346ed.

--- diabolowiki/diabolowiki/settings.py
+++ diabolowiki/diabolowiki/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auditlog',
     'tricks',
     'accounts',
 ]
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'auditlog.middleware.AuditlogMiddleware',
 ]
 
 ROOT_URLCONF = 'diabolowiki.urls'
