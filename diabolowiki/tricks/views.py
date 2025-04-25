140c5e7be22eb19984856ea1c5549b0ed3d1a26c
appletun091305 <apeartistemail@gmail.com>
2025-04-25 09:48:55 -0400

    history works if empty

--- diabolowiki/tricks/views.py
+++ diabolowiki/tricks/views.py
@@ -16,10 +16,14 @@ def alltricks(request):
 def trick(request, trickname):
     tr = Trick.objects.get(slug=trickname)
     first = tr.history.first()
+    if first.prev_record:
+        hist = first.diff_against(first.prev_record).changes
+    else:
+        hist = None
     context = {
         'trick':tr,
         'history': first,
-        'history_changes': first.diff_against(first.prev_record).changes,
+        'history_changes': hist,
     }
     return render(request, 'tricks/trickbase.html', context)
 
