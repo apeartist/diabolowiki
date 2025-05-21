d9c6c42df26db12997362945cc7a55c547e3ab27
appletun091305 <apeartistemail@gmail.com>
2025-05-21 10:25:44 -0400

    can upload profile pictures

--- diabolowiki/accounts/models.py
+++ diabolowiki/accounts/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    profile_picture = models.ImageField(blank=True, null=True)
+    profile_picture = models.ImageField(blank=True, null=True, upload_to="accounts/images/")
     description = models.TextField(blank=True, null=True)
 
